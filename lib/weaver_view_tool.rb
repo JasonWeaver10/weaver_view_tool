8bf4299251a89eb989392a58d5562aca8e19e5cf
jasontweaver <jasontweaver10@gmail.com>
2021-03-17 19:59:19 -0400

    initial commit

---
+++ lib/weaver_view_tool.rb
@@ -0,0 +1,7 @@
+require "weaver_view_tool/version"
+require "weaver_view_tool/renderer"
+
+module WeaverViewTool
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/weaver_view_tool/version.rb
@@ -0,0 +1,3 @@
+module WeaverViewTool
+  VERSION = "0.1.0"
+end
